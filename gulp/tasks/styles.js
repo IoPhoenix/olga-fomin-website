c341ea4f816e1ac6373d61dbeab6862e3379959a
IoPhoenix <olga.fomin26@gmail.com>
2017-11-07 15:56:41 -0800

    Started About section re-design

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,14 +4,15 @@ const gulp = require('gulp'),
     cssvars = require('postcss-simple-vars'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
-    mixins = require('postcss-mixins');
+    mixins = require('postcss-mixins'),
+    include = require('postcss-include');
 
 
 
 // use Autoprefixer to make css work in all browsers
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested,  autoprefixer]))
+        .pipe(postcss([cssImport, mixins, include, cssvars, nested,  autoprefixer]))
         .on('error', function(errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end')  
