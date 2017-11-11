94d25067acd3a2fe1553467779ceb1533b1abff4
IoPhoenix <olga.fomin26@gmail.com>
2017-11-10 22:59:26 -0800

    Completed footer styles

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,14 +5,15 @@ const gulp = require('gulp'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
     mixins = require('postcss-mixins'),
-    include = require('postcss-include');
+    include = require('postcss-include'),
+    hexrgba = require('postcss-hexrgba');
 
 
 
 // use Autoprefixer to make css work in all browsers
 gulp.task('styles', function() {
     return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, include, cssvars, nested,  autoprefixer]))
+        .pipe(postcss([cssImport, mixins, include, cssvars, nested, hexrgba, autoprefixer]))
         .on('error', function(errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end')  
