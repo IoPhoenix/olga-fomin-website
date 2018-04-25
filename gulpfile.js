64a3d373a1667408755963d049c9719672dae27d
IoPhoenix <olga.fomin26@gmail.com>
2018-04-25 13:42:56 -0700

    Added fallback for flexbox for older browsers

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,5 @@ require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
 require('./gulp/tasks/scripts');
 require('./gulp/tasks/build');
+require('./gulp/tasks/modernizr');
+// require('./gulp/tasks/build');
