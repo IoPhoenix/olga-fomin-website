3161f888ff6878338df348eddbda56836cc65d88
IoPhoenix <olga.fomin26@gmail.com>
2018-04-30 17:09:10 -0700

    Added fade in effect for lazyloading images

--- app/assets/scripts/modules/FadeInBackground.js
+++ app/assets/scripts/modules/FadeInBackground.js
@@ -11,7 +11,7 @@ class FadeInBackground {
     }
 
     showBackground() {
-        // fade in background image for home page on load
+        // fade in background image for home page slowly
         $('.fade-in-background').animate({opacity: 0}, 0)
                             .css({'background-image':
         'linear-gradient(rgba(24, 41, 86, .5), rgba(24, 41, 86, .7)), url(../../assets/images/flowers.jpeg)'})
--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -11065,7 +11065,7 @@ var FadeInBackground = function () {
     }, {
         key: 'showBackground',
         value: function showBackground() {
-            // fade in background image for home page on load
+            // fade in background image for home page slowly
             (0, _jquery2.default)('.fade-in-background').animate({ opacity: 0 }, 0).css({ 'background-image': 'linear-gradient(rgba(24, 41, 86, .5), rgba(24, 41, 86, .7)), url(../../assets/images/flowers.jpeg)' }).animate({ opacity: 1 }, 2500);
         }
     }]);
