ca7c661ddda8896330b5bbc06b8e867b33a633a6
IoPhoenix <olga.fomin26@gmail.com>
2018-04-30 21:34:04 -0700

    Finished build task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,68 +1,81 @@
-// const gulp = require('gulp'),
-//     imagemin = require('gulp-imagemin'),
-//     del = require('del'),
-//     rev = require('gulp-rev'),
-//     cssnano = require('gulp-cssnano'),
-//     usemin = require('gulp-usemin'),
-//     uglify = require('gulp-uglify'),
-//     browserSync = require('browser-sync').create();
+const gulp = require('gulp'),
+    imagemin = require('gulp-imagemin'),
+    del = require('del'),
+    rev = require('gulp-rev'),
+    cssnano = require('gulp-cssnano'), // compress css
+    usemin = require('gulp-usemin'), 
+    uglify = require('gulp-uglify'), // compress js
+    browserSync = require('browser-sync').create();
 
 
-// gulp.task('previewDist', function() {
-//     browserSync.init({
-//         notify: false,
-//         server: {
-//             baseDir: 'dist'
-//         }
-//     }); 
-// });
+// preview dist folder content in the browser
+gulp.task('previewDist', function() {
+    browserSync.init({
+        notify: false,
+        server: {
+            baseDir: 'dist'
+        }
+    }); 
+});
 
-// // optimize images and create new images folder in dist
-// gulp.task('compressImages', ['deleteDistFolder'], () => {
-//     return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
-//                 .pipe(imagemin({
-//                     progressive: true,
-//                     interlaced: true,
-//                     multipass: true
-//                 }))
-//                 .pipe(gulp.dest('./dist/assets/images'));
-// });
 
-// // delete dist folder each time we run gulp build
-// gulp.task('deleteDistFolder', () => {
-//     return del('./dist/');
-// });
+// delete dist folder each time we run gulp build
+gulp.task('deleteDistFolder', () => {
+    return del('./dist/');
+});
 
-// // copy any additional files into dist
-// gulp.task('copyGeneralFiles', ['deleteDistFolder'], function() {
-//     const paths = [
-//         './app/**/*',
-//          '!./app/index.html',
-//          '!./app/assets/images/**',
-//          '!./app/assets/videos/**',
-//          '!./app/assets/styles/**',
-//          '!./app/assets/scripts/**',
-//          '!./app/temp',
-//          '!./app/temp/**'
-//     ];
 
-//     return gulp.src(paths)
-//                 .pipe(gulp.dest('./dist'));
-// });
+// optimize images and create new images folder in dist
+gulp.task('compressImages', ['deleteDistFolder'], () => {
+    return gulp.src(['./app/assets/images/**/*', '!./app/assets/images/icons', '!./app/assets/images/icons/**/*'])
+                .pipe(imagemin({
+                    progressive: true,
+                    interlaced: true,
+                    multipass: true
+                }))
+                .pipe(gulp.dest('./dist/assets/images'));
+});
 
-// // copy index.html to dist folder;
-// // compress and revision js and css files;
-// // revision: appending content hash to filename
-// // so that browser re-downloads the file if it changes
-// gulp.task('usemin', ['styles', 'scripts'], () => {
-//     return gulp.src('./app/index.html')
-//                 .pipe(usemin({
-//                     // Revision and minify CSS:
-//                     css: [function() { return rev() }, function() { return cssnano()}],
-//                     // Revision and minify JavaScript:
-//                     js: [function() { return rev() }, function() { return uglify()}]
-//                 }))
-//                 .pipe(gulp.dest('./dist'));
-// });
+// optimize png icons
+gulp.task('compressIcons', ['deleteDistFolder'], () => {
+    return gulp.src(['./app/assets/images/icons/**/*'])
+                .pipe(imagemin({
+                    progressive: true,
+                    interlaced: true,
+                    multipass: true
+                }))
+                .pipe(gulp.dest('./dist/assets/images/icons'));
+});
 
-// gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'compressImages', 'usemin']);
+// copy any additional files into dist
+gulp.task('copyGeneralFiles', ['deleteDistFolder'], () => {
+    const paths = [
+        './app/**/*',
+         '!./app/index.html',
+         '!./app/assets/images/**',
+         '!./app/assets/styles/**',
+         '!./app/assets/scripts/**',
+         '!./app/temp',
+         '!./app/temp/**'
+    ];
+
+    return gulp.src(paths)
+                .pipe(gulp.dest('./dist'));
+});
+
+// copy index.html to dist folder;
+// compress and revision js and css files;
+// revision: appending content hash to filename
+// so that browser re-downloads the file if it changes
+gulp.task('usemin', ['styles', 'scripts'], () => {
+    return gulp.src('./app/index.html')
+                .pipe(usemin({
+                    // Revision and minify CSS:
+                    css: [function() { return rev() }, function() { return cssnano()}],
+                    // Revision and minify JavaScript:
+                    js: [function() { return rev() }, function() { return uglify()}]
+                }))
+                .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['deleteDistFolder', 'copyGeneralFiles', 'compressImages', 'compressIcons', 'usemin']);
