c8dda4d5bf88f8241e774a6f131faa5ef930896d
Olga Fomin <olga.fomin26@gmail.com>
2019-01-15 23:37:25 -0800

    Remove slick slider on smallest screens

--- app/assets/scripts/modules/ProjectContent.js
+++ app/assets/scripts/modules/ProjectContent.js
@@ -37,7 +37,7 @@ class ProjectContent {
             mobileFirst: true,
             responsive: [{
            
-                breakpoint: 992,
+                breakpoint: 1024,
                 settings: {
                   slidesToShow: 3,
                   slidesToScroll: 3,
@@ -55,12 +55,13 @@ class ProjectContent {
         
               }, {
                 breakpoint: 0,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    dots: true,
-                    arrows: false
-                  }
+                // settings: {
+                //     slidesToShow: 1,
+                //     slidesToScroll: 1,
+                //     dots: true,
+                //     arrows: false
+                //   }
+                    settings: "unslick"
            
               }]
         });
