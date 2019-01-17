d03714abb02013746578a4bbf505ffc10effe4ba
Olga Fomin <olga.fomin26@gmail.com>
2019-01-16 21:20:41 -0800

    Remove slider arrows on small screens

--- app/assets/scripts/modules/ProjectContent.js
+++ app/assets/scripts/modules/ProjectContent.js
@@ -38,25 +38,32 @@ class ProjectContent {
             responsive: [{
            
                 breakpoint: 1024,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  dots: true
-                }
-        
-              }, {
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        dots: true
+                    }
+                }, {
+           
+                breakpoint: 768,
+                    settings: {
+                        arrows: true,
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                }, {
            
                 breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                  dots: true
-                }
-        
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        dots: true,
+                        arrows: false
+                    }
               }, {
                 breakpoint: 0,
                 // remove slick slider:
-                settings: "unslick"
+                settings: "unslick",
               }]
         });
     }
